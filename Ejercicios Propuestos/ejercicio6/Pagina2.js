6534a9e1f3f47717d8c1fde04a15a300e74eb6ec
CristhianBravo865 <cristhian.bravo8657@gmail.com>
2025-04-28 11:34:38 -0500

    feat: ejercicio6/Pagina2.js (logica para operaciones aritmeticas - falta la raiz)

--- Ejercicios Propuestos/ejercicio6/Pagina2.js
+++ Ejercicios Propuestos/ejercicio6/Pagina2.js
@@ -1,15 +1,32 @@
 const botonCalcular = document.getElementById('Boton_calcular');
 
 botonCalcular.addEventListener('click', () => {
-    const numero1 = document.getElementById('input_numero1').value;
-    const numero2 = document.getElementById('input_numero2').value;
+    const numero1 = parseFloat(document.getElementById('input_numero1').value);
+    const numero2 = parseFloat(document.getElementById('input_numero2').value);
     const operador = document.getElementById('operador').value;
     const resultadoDiv = document.getElementById('resultado');
 
     let resultado;
 
-    // logica de operaciones
-    resultado = "";
+    switch (operador) {
+        case '+':
+            resultado = numero1 + numero2;
+            break;
+        case '-':
+            resultado = numero1 - numero2;
+            break;
+        case '*':
+            resultado = numero1 * numero2;
+            break;
+        case '/':
+            resultado = numero2 !== 0 ? (numero1 / numero2) : 'Error: División entre cero';
+            break;
+        case '^':
+            resultado = Math.pow(numero1, numero2);
+            break;
+        default:
+            resultado = 'Operacion no implementada';
+    }
 
     resultadoDiv.textContent = `Resultado: ${resultado}`;
 });
